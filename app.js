cc8bf3d5f38dc3b01863aa9bcd93a0c3d2c67ff6
HS-developers <h.azaim1977@gmail.com>
2024-11-09 15:47:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCnRLUzLraNE-AR94ZlRGIAFOKks74ZtyQ",
-  authDomain: "kenz--project.firebaseapp.com",
-  databaseURL: "https://kenz--project-default-rtdb.firebaseio.com",
-  projectId: "kenz--project",
-  storageBucket: "kenz--project.firebasestorage.app",
-  messagingSenderId: "435317870255",
-  appId: "1:435317870255:web:f521650dcfeb63a7378e5a",
-  measurementId: "G-FX6BSCQ8KQ"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCnRLUzLraNE-AR94ZlRGIAFOKks74ZtyQ",
+    authDomain: "kenz--project.firebaseapp.com",
+    databaseURL: "https://kenz--project-default-rtdb.firebaseio.com",
+    projectId: "kenz--project",
+    storageBucket: "kenz--project.firebasestorage.app",
+    messagingSenderId: "435317870255",
+    appId: "1:435317870255:web:f521650dcfeb63a7378e5a",
+    measurementId: "G-FX6BSCQ8KQ"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-try {
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app); 
-    const database = getDatabase(app); 
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 
     function updateCount(type, id) {
         const likeIcon = document.getElementById(`like-${id}`);
