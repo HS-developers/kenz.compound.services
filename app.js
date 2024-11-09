695d1c4504e5ad3ef51ca6361a09eeadbf8d9a5b
HS-developers <h.azaim1977@gmail.com>
2024-11-09 17:49:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getDatabase, ref, get, set, onValue } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js"; // Import Analytics
+//import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js"; // Import Analytics
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,7 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app); // Initialize Analytics
+//const analytics = getAnalytics(app); // Initialize Analytics
 const database = getDatabase(app); // Initialize Database
 
 function updateCount(type, id) {
